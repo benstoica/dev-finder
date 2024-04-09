bf40cb2282cb1e2a7e7ec4ae8dae2a4e94ecae48
Ben Stoica <bennystoica@gmail.com>
2024-04-09 18:18:10 -0400

    header work

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,20 +2,64 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon, SearchCodeIcon } from "lucide-react";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+
+const AccountDropdown = () => {
+  const session = useSession();
+  const isLoggedIn = !!session.data;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="link">
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user?.image ?? ""} />
+            <AvatarFallback>{session.data?.user.name}</AvatarFallback>
+          </Avatar>
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className="mr-2" />
+            Sign Out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            <LogInIcon className="mr-2" />
+            Sign In
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export const Header = () => {
-  const session = useSession();
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => signIn("google")}>Sign In</Button>
-        )}
-        <ModeToggle />
+    <header className="py-2 bg-gray-100 dark:bg-gray-900 container mx-auto">
+      <div className="flex justify-between items-center">
+        <Link href="/">
+          <div className="flex gap-2 items-center text-xl hover:underline">
+            <SearchCodeIcon width="60" height="60" />
+            DevFinder
+          </div>
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
